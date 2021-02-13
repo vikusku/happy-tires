286ff9d254db2e9a845b390d3b5bde9d623b7b54
Viktoriia Kuznietsova <viktoriia.kuznietsova@sok.fi>
2021-02-13 23:48:59 +0200

    Init project

---
+++ src/main/java/com/github/vikusku/happytires/HappyTiresApplication.java
@@ -0,0 +1,13 @@
+package com.github.vikusku.happytires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTiresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTiresApplication.class, args);
+	}
+
+}
